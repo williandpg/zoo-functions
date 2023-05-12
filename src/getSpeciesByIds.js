5c68d3824093c79a9bbfc34e00cf5635aba31a22
Willian Gonçalves <willian.p.g@outlook.com>
2023-05-12 15:06:26 -0300

    requisito um finalizado

--- src/getSpeciesByIds.js
+++ src/getSpeciesByIds.js
@@ -1,8 +1,8 @@
 const data = require('../data/zoo_data');
 
 const getSpeciesByIds = (...ids) => {
-  const SpeciesByIds = ids.map((id) => data.species.find((specie) => specie.id === id));
-  return SpeciesByIds;
+  const speciesByIds = ids.map((id) => data.species.find((specie) => specie.id === id));
+  return speciesByIds;
 };
 
 module.exports = getSpeciesByIds;
