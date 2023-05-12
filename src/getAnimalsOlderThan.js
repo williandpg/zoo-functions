cbf0185a63ef04f851e1c0b36c7ef53cbf7cd643
Willian Gonçalves <willian.p.g@outlook.com>
2023-05-12 15:37:15 -0300

    requisito dois iniciado

--- src/getAnimalsOlderThan.js
+++ src/getAnimalsOlderThan.js
@@ -2,6 +2,8 @@ const data = require('../data/zoo_data');
 
 const getAnimalsOlderThan = (animal, age) => {
   // seu código aqui
+  const minimumAge = data.species.find((specie) => specie.name === animal);
+  return minimumAge.residents.every((resident) => resident.age >= age);
 };
 
-module.exports = getAnimalsOlderThan;
+module.exports = getAnimalsOlderThan
--- src/getSpeciesByIds.js
+++ src/getSpeciesByIds.js
@@ -1,6 +1,7 @@
 const data = require('../data/zoo_data');
 
 const getSpeciesByIds = (...ids) => {
+  // seu código aqui
   const speciesByIds = ids.map((id) => data.species.find((specie) => specie.id === id));
   return speciesByIds;
 };
