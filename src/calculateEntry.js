d63edb2897f700f836bec716c411d288f6d73fd1
Willian Gonçalves <willian.p.g@outlook.com>
2023-05-12 21:11:54 -0300

    requisito seis, sete e oito iniciados e finalizados

--- src/calculateEntry.js
+++ src/calculateEntry.js
@@ -2,10 +2,20 @@ const data = require('../data/zoo_data');
 
 const countEntrants = (entrants) => {
   // seu código aqui
+  const child = entrants.filter((people) => people.age < 18);
+  const adult = entrants.filter((people) => people.age >= 18 && people.age < 50);
+  const senior = entrants.filter((people) => people.age >= 50);
+
+  return { child: child.length, adult: adult.length, senior: senior.length };
 };
 
 const calculateEntry = (entrants) => {
   // seu código aqui
+  if (!entrants || Object.keys(entrants).length === 0) {
+    return 0;
+  }
+  const count = countEntrants(entrants);
+  return count.child * 20.99 + count.adult * 49.99 + count.senior * 24.99;
 };
 
 module.exports = { calculateEntry, countEntrants };
--- test/handlerElephants.test.js
+++ test/handlerElephants.test.js
@@ -1,3 +1,25 @@
 const handlerElephants = require('../src/handlerElephants');
 
-describe('Testes da função HandlerElephants', () => {});
+describe('Testes da função HandlerElephants', () => {
+  it('Retorna a quantidade de elefantes', () => {
+    expect(handlerElephants('count')).toEqual(4);
+  });
+  it('Retorna um array com a relação dos nomes de todos os elefantes;', () => {
+    expect(handlerElephants('names')).toContain('Jefferson');
+  });
+  it('Retorna a média de idade dos elefantes', () => {
+    expect(handlerElephants('averageAge')).toBeCloseTo(10.5);
+  });
+  it('Retorna a localização dos elefantes dentro do Zoológico', () => {
+    expect(handlerElephants('location')).toEqual(('NW'));
+  });
+  it('Retorna a popularidade dos elefantes', () => {
+    expect(handlerElephants('popularity')).toBeGreaterThanOrEqual(5);
+  });
+  it('Retorna um array com a relação de dias em que é possível visitar os elefantes', () => {
+    expect(handlerElephants('availability')).toEqual(['Friday', 'Saturday', 'Sunday', 'Tuesday']);
+  });
+  it('Não passando argumentos a função deve retornar undefined', () => {
+    expect(handlerElephants()).toBeUndefined();
+  });
+});
