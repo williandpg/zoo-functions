6853fe79ee8a959dfd99ae087605b0ac48a0dc5e
Willian Gonçalves <willian.p.g@outlook.com>
2023-05-15 19:23:05 -0300

    requisito onze iniciado

--- src/getEmployeesCoverage.js
+++ src/getEmployeesCoverage.js
@@ -1,5 +1,13 @@
 const data = require('../data/zoo_data');
 
+const employeeResp = (employeeId) => {
+  const employee = data.employees.find((idEmployee) =>
+    (idEmployee.id === employeeId.id) || (idEmployee.firstName === employeeId.name));
+  if (employee === undefined) {
+    throw new Error('Informações inválidas');
+  }
+};
+
 const getEmployeesCoverage = () => {
   // seu código aqui
 };
